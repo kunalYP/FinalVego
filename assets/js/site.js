0668114fa53215f1f4ba676c86d15a3b9d3b73a2
kunalYP <69045235+kunalYP@users.noreply.github.com>
2020-10-21 10:02:23 -0700

    Add files via upload

---
+++ assets/js/site.js
@@ -0,0 +1,13 @@
+function sidebar_open() {
+
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+
+
+}
+
+function sidebar_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+
+}
