1b802d6df6a4c36d0ddd19b96c4226dfe405c3b7
kunalYP <69045235+kunalYP@users.noreply.github.com>
2020-11-09 04:11:13 -0800

    Add files via upload

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,57 +1,108 @@
 function sidebar_open() {
-
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-
-
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
 }
 
 function sidebar_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
 }
 function addLocationsidebar_open() {
-
-    document.getElementById("addLocationSidebar").style.display = "block";
-    document.getElementById("myOverlay2").style.display = "block";
-
-
+  document.getElementById("addLocationSidebar").style.display = "block";
+  document.getElementById("myOverlay2").style.display = "block";
 }
 
 function addLocationsidebar_close() {
-    document.getElementById("addLocationSidebar").style.display = "none";
-    document.getElementById("myOverlay2").style.display = "none";
-
+  document.getElementById("addLocationSidebar").style.display = "none";
+  document.getElementById("myOverlay2").style.display = "none";
 }
 function sortsidebar_open() {
-
   document.getElementById("sortsidebar").style.display = "block";
   document.getElementById("myOverlay3").style.display = "block";
-
-
 }
 
 function sortsidebar_close() {
   document.getElementById("sortsidebar").style.display = "none";
   document.getElementById("myOverlay3").style.display = "none";
-
 }
 function filtersidebar_open() {
-
   document.getElementById("filtersidebar").style.display = "block";
   document.getElementById("myOverlay4").style.display = "block";
-
-
 }
 
 function filtersidebar_close() {
   document.getElementById("filtersidebar").style.display = "none";
   document.getElementById("myOverlay4").style.display = "none";
+}
+
+
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  document.getElementsByClassName("step")[currentTab].className += " finish";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
 
 }
 
-window.onscroll = function() {myFunction()};
+
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
+
+
+
+
+
+
+
+
+
+window.onscroll = function () {
+  myFunction()
+};
 
 var navbar = document.getElementById("searchbox");
 var filters = document.getElementById("filtersection");
